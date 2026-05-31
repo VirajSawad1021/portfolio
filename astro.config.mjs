bf7716b372c10f1443bdc1865053f03f2d5fde9b
test <test@test.com>
2026-05-31 12:46:00 +0545

    Consolidate styling under a single entry point by setting applyBaseStyles false and explicitly prepending tailwind directives to global.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://virajsawad.com.np',
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx()
+  ],
 });
